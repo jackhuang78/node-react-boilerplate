a0a9f02ae1438166bdbce15593426c37540f0409
Jack Huang <jack.huang78@gmail.com>
2016-10-20 21:57:20 -0700

    more content in example

--- src/views/Hello.js
+++ src/views/Hello.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Panel, PageHeader} from 'react-bootstrap';
+import {Panel, PageHeader, Jumbotron} from 'react-bootstrap';
 
 class Hello extends React.Component {
 	render() {
 		return (
-			<Panel>
-				<PageHeader>Hello world!</PageHeader>
+			<Panel style={{margin: 30}}>
+				<Jumbotron style={{'padding': 30}}>
+			    <h1>Hello, world!</h1>
+			    <p>Powered by node.js, react, and bootstrap.</p>
+			  </Jumbotron>
 			</Panel>
 		);
 	}
-}
+};
 
 export default Hello;
