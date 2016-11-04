e58f78a7a0a1c6af8a11267dbb58d78917c1a12c
Jack Huang <jack.huang78@gmail.com>
2016-11-04 01:32:52 -0700

    travis test

--- spec/App.spec.js
+++ spec/App.spec.js
@@ -1,7 +1,7 @@
 import chai, {expect,AssertionError} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import {get, post, put, delete} from 'needle';
-
+import log4js from 'log4js';
 let logger = log4js.getLogger(path.basename(__filename));
 logger.info(__filename);
 
